b7432aa94f9e64193c4d56bc66ba3bde66f0024b
Mohamed Fahim <mohamed.fahim@hpe.com>
2016-05-06 15:24:41 +0530

    New:Dev:ospfv2 FT testcases
    Change-Id: I4ac830d4543edb6e9cb78ea602abb51b0e464057
    Signed-off-by: Mohamed Fahim <mohamed.fahim@hpe.com>

--- ospfd/ops-tests/feature/ospf_configs.py
+++ ospfd/ops-tests/feature/ospf_configs.py
@@ -21,6 +21,7 @@
 
 
 import time
+import re
 
 
 # This function will return hello timer from
@@ -36,6 +37,16 @@ def get_hello_timer(switch_id):
         hello_interval = 0
 
     return hello_interval
+
+
+# Function to verify ospf neighbor priority
+def verify_ospf_priority(switch_id, priority):
+    output = switch_id.libs.vtysh.show_ip_ospf_interface()
+    if output:
+        priority_value = output['priority']
+        if priority_value == priority:
+            return True
+    return False
 
 
 # function to get neighbor state DR/BDR/DROTHER
@@ -142,7 +153,7 @@ def verify_ospf_adjacency(switch_id, neighbor_id):
 
 # Function to verify adjacency between two switches
 def wait_for_adjacency(switch_id, neighbor_id, condition=True):
-    wait_time = get_hello_timer(switch_id) + 35
+    wait_time = get_hello_timer(switch_id) + 50
     for i in range(0, wait_time):
         found = verify_ospf_adjacency(switch_id, neighbor_id)
         if found == condition:
@@ -151,6 +162,37 @@ def wait_for_adjacency(switch_id, neighbor_id, condition=True):
             time.sleep(1)
 
     return found
+
+
+# Function to verify VLINK0 between switches
+def verify_virtual_links(raw_result, neighbor_id):
+    neighbor_re = (
+        r'(?P<neighbor_id>[^ ]+)\s*(?P<priority>[^ ]+)\s*'
+        '(?P<state>[^ ]+)'
+        r'\s*(?P<dead_time>[^ ]+)\s*(?P<address>[^ ]+)\s*'
+        '(?P<interface>[^ ]+)'
+        r'\s*(?P<rxmtl>[^ ]+)\s*(?P<rqstl>[^ ]+)\s*(?P<dbsml>[^ ])'
+    )
+    result = {}
+    pattern_found = False
+    for line in raw_result.splitlines():
+        if (pattern_found is True):
+            re_result = re.search(neighbor_re, line)
+            if (re_result):
+                partial = re_result.groupdict()
+                result[partial['interface']] = partial
+        else:
+            re_result = re.search('-+\s*-+', line)
+            if (re_result):
+                pattern_found = True
+
+    if result:
+        if 'VLINK0' in result.keys():
+            output = result['VLINK0']
+            if (output['neighbor_id'] == neighbor_id):
+                return True
+        else:
+            return False
 
 
 # Function to wait until 2-Way or greater state
@@ -166,6 +208,8 @@ def wait_for_2way_state(switch_id, neighbor_id):
                 state = str1[0]
                 if (state == "ExStart" or state == "Exchange" or state ==
                         "Loading" or state == "Full"):
+                    # waiting here for DR/DBR/DROther election
+                    time.sleep(10)
                     return True
                 else:
                     time.sleep(1)
@@ -174,7 +218,7 @@ def wait_for_2way_state(switch_id, neighbor_id):
 
 
 # To verify the router-type from
-# show_ip_ospf command
+# show ip ospf command
 def verify_router_type(switch_id):
     output = switch_id.libs.vtysh.show_ip_ospf()
     if output:
@@ -187,10 +231,23 @@ def verify_router_type(switch_id):
         return 0
 
 
+# Wait for routes to be updated
+def wait_for_routes(switch_id, ip_addr, prefix_addr, condition=True):
+    wait_time = 40
+    for i in range(0, wait_time):
+        found = verify_route(switch_id, ip_addr, prefix_addr)
+        if found == condition:
+            return found
+        else:
+            time.sleep(1)
+
+    return found
+
+
 # Verify the connected routes using
 # show rib command
 def verify_route(switch_id, ip_addr, prefix_addr):
-    flag = 0
+    result = 0
     output = switch_id.libs.vtysh.show_rib()
     if output:
         list_entries = output['ipv4_entries']
@@ -198,11 +255,54 @@ def verify_route(switch_id, ip_addr, prefix_addr):
         for i in range(0, n):
             partial = output['ipv4_entries'][i]
             if partial['id'] == ip_addr and partial['prefix'] == prefix_addr:
-                flag = 1
-        if(flag == 1):
+                result = 1
+        if(result == 1):
             return True
         else:
             return False
+
+
+# Wait for routes and distance to be updated
+def wait_for_route_distance(switch_id, ip_addr, prefix_addr,
+                            cost, condition=True):
+    wait_time = 40
+    for i in range(0, wait_time):
+        found = verify_route_distance(switch_id, ip_addr, prefix_addr, cost)
+        if found == condition:
+            return found
+        else:
+            time.sleep(1)
+
+    return found
+
+
+# Verify the routes with distance metrics using
+# show rib command
+def verify_route_distance(switch_id, ip_addr, prefix_addr, cost):
+    output = switch_id.libs.vtysh.show_rib()
+    if output:
+        list_entries = output['ipv4_entries']
+        n = len(list_entries)
+        for i in range(0, n):
+            partial = output['ipv4_entries'][i]
+            metric = output['ipv4_entries'][i]['next_hops'][0]
+            if partial['id'] == ip_addr and partial['prefix'] == prefix_addr:
+                if metric['distance'] == cost:
+                    return True
+
+    return False
+
+
+# Function to verify OSPF best path
+def wait_for_best_route(switch_id, ip_addr, distance):
+    output = switch_id.libs.vtysh.show_ip_route()
+    if output:
+        route_length = len(output)
+        for i in range(0, route_length):
+            if (output[i]['id']) == ip_addr:
+                if (output[i]['next_hops'][0]['distance']) == distance:
+                    return True
+    return False
 
 
 # OSPF functions to handle unconfiguration commands

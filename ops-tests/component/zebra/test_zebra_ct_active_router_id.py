a2d05f4a9e248ccad2cb1be353ca0375c9bef8fc
Hitesh Padekar <hitesh.mar.padekar@hpe.com>
2016-06-08 17:12:52 -0700

    Remove communication between BGP and Zebra deamon for the router_id update.
    TG-714
    Severity : High
    Description: when the router ID is not configured in BGP router
    configuration, one of the interface IP should be used as a Router ID.
    This commit will remove the BGP router_id message passing communication
    with Zebra. Zebra is writing active router id into the VRF table's
    active_router_id column and BGPd is using this for setting BGP router's
    router_id.
    
    Change-Id: I8af7ad966a9137ee95471f108fd9dcde47e62c51
    Signed-off-by: Hitesh Padekar <hitesh.mar.padekar@hpe.com>

---
+++ ops-tests/component/zebra/test_zebra_ct_active_router_id.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+# (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+# GNU Zebra is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any
+# later version.
+#
+# GNU Zebra is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Zebra; see the file COPYING.  If not, write to the Free
+# Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+
+TOPOLOGY = """
+#
+# +-------+
+# |  sw1  |
+# +-------+
+#
+
+# Nodes
+[type=openswitch name="Switch 1"] sw1
+
+"""
+from time import sleep
+
+#  This is basic configuration required for the test, it verifies zebra
+#  deamon is running and configures one interface with IPv4 address.
+def configure_interface(sw1, step):
+    interface_addr1 = "9.0.0.1"
+    masklen = "8"
+
+    step("1-Verifying zebra processes...")
+    pid = sw1("pgrep -f zebra", shell='bash')
+    pid = pid.strip()
+    assert pid != "" and pid is not None
+
+    step("2-Applying interface configurations")
+    sw1("configure terminal")
+    sw1("interface 1")
+    sw1("no shutdown")
+    sw1("ip address {}/{}".format(interface_addr1, masklen))
+    sw1("exit")
+    sleep(1)
+
+#  This test verifies active_router_id column in VRF table is same
+#  as the interface 1 IPv4 address.
+def verify_active_router_id(sw1, step):
+    step("3-Verifying Active Router-ID is one of the interface ip")
+    active_router_id1 = "9.0.0.1"
+    output = sw1("ovsdb-client dump VRF", shell='bash')
+    assert active_router_id1 in output
+
+#  This test configures the loopback interface and verifies that
+#  loopback IPv4 address is used as active_router_id.
+def verify_loopback_interface(sw1, step):
+    interface_addr1 = "9.0.0.1"
+    masklen1 = "8"
+    masklen2 = "24"
+    step("4-Verifying Loopback IP gets higher priority")
+    loopback_ip = "9.0.1.3"
+    active_router_id3 = "9.0.1.3"
+    sw1("configure terminal")
+    sw1("interface 1")
+    sw1("shutdown")
+    sw1("no ip address {}/{}".format(interface_addr1, masklen1))
+    sw1("exit")
+    sw1("interface loopback 3")
+    sw1("ip address {}/{}".format(loopback_ip, masklen2))
+    sw1("exit")
+    sleep(1)
+    output = sw1("ovsdb-client dump VRF", shell='bash')
+    assert active_router_id3 in output
+
+#  This test verifies that unconfiguring the loopback interface which
+#  was used as active_router_id will change it to any other L3
+#  interface IPv4 address.
+def verify_unconfigure_loopback_interface(sw1, step):
+    masklen1 = "8"
+    masklen2 = "24"
+    interface_addr1 = "9.0.0.1"
+    loopback_ip = "9.0.1.3"
+    active_router_id1 = "9.0.0.1"
+    step("4-Verify deleting loopback interface")
+    sw1("configure terminal")
+    sw1("interface 1")
+    sw1("no shutdown")
+    sw1("ip address {}/{}".format(interface_addr1, masklen1))
+    sw1("exit")
+    sw1("interface loopback 3")
+    sw1("no ip address {}/{}".format(loopback_ip, masklen2))
+    sw1("exit")
+    sleep(1)
+    output = sw1("ovsdb-client dump VRF", shell='bash')
+    assert active_router_id1 in output
+
+#  This test verifies that unconfiguring the L3 interface which was
+#  used as active_router_id changes it to any other available L3
+#  interface IPv4 address
+def verify_unconfigure_interface(sw1, step):
+    interface_addr1 = "9.0.0.1"
+    interface_addr2 = "9.0.0.2"
+    active_router_id2 = "9.0.0.2"
+    masklen = "8"
+    step("5-Verify deleting one of the interface")
+    sw1("configure terminal")
+    sw1("interface 1")
+    sw1("shutdown")
+    sw1("no ip address {}/{}".format(interface_addr1, masklen))
+    sw1("exit")
+    sw1("interface 2")
+    sw1("no shutdown")
+    sw1("ip address {}/{}".format(interface_addr2, masklen))
+    sw1("exit")
+    sleep(1)
+    output = sw1("ovsdb-client dump VRF", shell='bash')
+    assert active_router_id2 in output
+
+def test_zebra_ct_active_router_id(topology, step):
+    sw1 = topology.get("sw1")
+    assert sw1 is not None
+    configure_interface(sw1, step)
+    verify_active_router_id(sw1, step)
+    verify_loopback_interface(sw1, step)
+    verify_unconfigure_loopback_interface(sw1, step)
+    verify_unconfigure_interface(sw1, step)

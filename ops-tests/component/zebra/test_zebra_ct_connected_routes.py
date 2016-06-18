574b7ba19b3f1cf714ecac8a832026be377db7b1
Gaurav Gupta <gaurav.gupta4@hpe.com>
2016-06-17 17:08:45 -0700

    Enabling connected routes CT in ops-zebra
    TG-1308
    Tags: fix, usr
    
    Change-Id: I87a74071fea04d9f06155c7b703f0b0374e8d022
    Signed-off-by: Gaurav Gupta <gaurav.gupta4@hpe.com>

--- ops-tests/component/zebra/test_zebra_ct_connected_routes.py
+++ ops-tests/component/zebra/test_zebra_ct_connected_routes.py
@@ -3144,8 +3144,6 @@ def no_routing_or_delete_layer3_interfaces(sw1, sw2, step):
                     rib_ipv6_lag_connected_route_secondary)
 
 
-@mark.skipif(True, reason="Waiting for zebra modular frame changes. \
-                           Skipping zebra connected route CT")
 def test_zebra_ct_connected_routes(topology, step):
     sw1 = topology.get("sw1")
     sw2 = topology.get("sw2")

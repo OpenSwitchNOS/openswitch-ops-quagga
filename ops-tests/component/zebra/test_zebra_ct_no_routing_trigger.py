540452402a501fda2006c2455abde331a24b15c7
Gaurav Gupta <gaurav.gupta4@hpe.com>
2016-06-21 15:33:28 -0700

    ops-zebra selecting routes in OVSDB erronously
    This fixes the following two issues:-
    - ops-zebra selecting routes in OVSDB without causing the next-hop
      selection criteria to run. This is causing the backup hops to get
      selected in OVSDB in case of interface state change triggers.
    
    - In case of "no routing/interface delete" triggers, ops-zebra would
      delete BGP and OSPF protocol next-hops from DB. ops-zebra just needs
      to process the next-hops from static routes.
    
    - In case of deletion of L3 interface using "no interface loopback 1",
      zebra does not cleanup the static routes routing via that interface.
      ops-zebra should trigger a clean-up such next-hops.
    
    TG-1440
    Tags: fix, usr
    
    Change-Id: Iaf8d529b08ed7feb8ae0765617e557e6626613c7
    Signed-off-by: Gaurav Gupta <gaurav.gupta4@hpe.com>

--- ops-tests/component/zebra/test_zebra_ct_no_routing_trigger.py
+++ ops-tests/component/zebra/test_zebra_ct_no_routing_trigger.py
@@ -366,6 +366,7 @@ def no_routing_trigger_static_routes(sw1, sw2, step):
     sw1("interface {}".format(sw1.ports["if01"]))
     sw1("no routing")
     sw1("exit")
+
     step("Entering interface for link 4 SW1, to execute 'no routing'")
 
     # Make the L3 interface 4 as L2 by executing "no routing"
@@ -785,6 +786,318 @@ def routing_trigger_static_routes(sw1, sw2, step):
                                                     nexthop='2')
 
 
+# This test case configures few L3 sub-interfaces and adds few IPv4/IPv6
+# static routes with sub-interfaces as next-hops.
+def add_static_routes_via_l3_sub_interfaces(sw1, sw2, step):
+
+    # Add L3 sub-interfaces within interface 1
+    sw1("interface 1.1")
+    sw1("encapsulation dot1Q 2")
+    sw1("ip address 10.10.10.10/24")
+    sw1("ipv6 address 100:100::1/64")
+    sw1("no shutdown")
+    sw1("exit")
+
+    sw1("interface 1.2")
+    sw1("encapsulation dot1Q 3")
+    sw1("ip address 11.11.11.11/24")
+    sw1("ipv6 address 101:101::1/64")
+    sw1("no shutdown")
+    sw1("exit")
+
+    # Add L3 sub-interfaces within interface 4
+    sw1("interface 4.1")
+    sw1("encapsulation dot1Q 4")
+    sw1("ip address 40.40.40.40/24")
+    sw1("ipv6 address 400:400::1/64")
+    sw1("no shutdown")
+    sw1("exit")
+
+    sw1("interface 4.2")
+    sw1("encapsulation dot1Q 5")
+    sw1("ip address 41.41.41.41/24")
+    sw1("ipv6 address 401:401::1/64")
+    sw1("no shutdown")
+    sw1("exit")
+
+    # Add IPv4 ECMP static route via the sub-interfaces
+    sw1("ip route 173.0.0.1/32 1.1")
+    sw1("ip route 173.0.0.1/32 11.11.11.13")
+    sw1("ip route 173.0.0.1/32 41.41.41.43")
+
+    # Add IPv4 non-ECMP static route via the sub-interfaces
+    sw1("ip route 183.0.0.1/32 1.1")
+
+    # Add IPv6 ECMP static route via the sub-interfaces
+    sw1("ipv6 route 4234:4234::1/128 1.1")
+    sw1("ipv6 route 4234:4234::1/128 100:100::6")
+    sw1("ipv6 route 4234:4234::1/128 4.1")
+
+    # Add IPv6 non-ECMP static route via the sub-interfaces
+    sw1("ipv6 route 5234:5234::1/128 1.1")
+
+    # Populate the expected RIB ("show rib") route dictionary for the
+    # route 173.0.0.1/32 and its next-hops.
+    rib_ipv4_static_route1 = dict()
+    rib_ipv4_static_route1['Route'] = '173.0.0.1/32'
+    rib_ipv4_static_route1['NumberNexthops'] = '3'
+    rib_ipv4_static_route1['1.1'] = dict()
+    rib_ipv4_static_route1['1.1']['Distance'] = '1'
+    rib_ipv4_static_route1['1.1']['Metric'] = '0'
+    rib_ipv4_static_route1['1.1']['RouteType'] = 'static'
+    rib_ipv4_static_route1['11.11.11.13'] = dict()
+    rib_ipv4_static_route1['11.11.11.13']['Distance'] = '1'
+    rib_ipv4_static_route1['11.11.11.13']['Metric'] = '0'
+    rib_ipv4_static_route1['11.11.11.13']['RouteType'] = 'static'
+    rib_ipv4_static_route1['41.41.41.43'] = dict()
+    rib_ipv4_static_route1['41.41.41.43']['Distance'] = '1'
+    rib_ipv4_static_route1['41.41.41.43']['Metric'] = '0'
+    rib_ipv4_static_route1['41.41.41.43']['RouteType'] = 'static'
+
+    # Populate the expected FIB ("show ip route") route dictionary for the
+    # route 173.0.0.1/32 and its next-hops.
+    route_ipv4_static_route1 = rib_ipv4_static_route1
+
+    # Populate the expected RIB ("show rib") route dictionary for the
+    # route 183.0.0.1/32 and its next-hops.
+    rib_ipv4_static_route2 = dict()
+    rib_ipv4_static_route2['Route'] = '183.0.0.1/32'
+    rib_ipv4_static_route2['NumberNexthops'] = '1'
+    rib_ipv4_static_route2['1.1'] = dict()
+    rib_ipv4_static_route2['1.1']['Distance'] = '1'
+    rib_ipv4_static_route2['1.1']['Metric'] = '0'
+    rib_ipv4_static_route2['1.1']['RouteType'] = 'static'
+
+    # Populate the expected FIB ("show ip route") route dictionary for the
+    # route 183.0.0.1/32 and its next-hops.
+    route_ipv4_static_route2= rib_ipv4_static_route2
+
+    # Populate the expected RIB ("show rib") route dictionary for the
+    # route 4234:4234::1/128 and its next-hops.
+    rib_ipv6_static_route1 = dict()
+    rib_ipv6_static_route1['Route'] = '4234:4234::1/128'
+    rib_ipv6_static_route1['NumberNexthops'] = '3'
+    rib_ipv6_static_route1['1.1'] = dict()
+    rib_ipv6_static_route1['1.1']['Distance'] = '1'
+    rib_ipv6_static_route1['1.1']['Metric'] = '0'
+    rib_ipv6_static_route1['1.1']['RouteType'] = 'static'
+    rib_ipv6_static_route1['100:100::6'] = dict()
+    rib_ipv6_static_route1['100:100::6']['Distance'] = '1'
+    rib_ipv6_static_route1['100:100::6']['Metric'] = '0'
+    rib_ipv6_static_route1['100:100::6']['RouteType'] = 'static'
+    rib_ipv6_static_route1['4.1'] = dict()
+    rib_ipv6_static_route1['4.1']['Distance'] = '1'
+    rib_ipv6_static_route1['4.1']['Metric'] = '0'
+    rib_ipv6_static_route1['4.1']['RouteType'] = 'static'
+
+    # Populate the expected FIB ("show ipv6 route") route dictionary for the
+    # route 4234:4234::1/128 and its next-hops.
+    route_ipv6_static_route1 = rib_ipv6_static_route1
+
+    # Populate the expected RIB ("show rib") route dictionary for the
+    # route 5234:5234::1/128 and its next-hops.
+    rib_ipv6_static_route2 = dict()
+    rib_ipv6_static_route2['Route'] = '5234:5234::1/128'
+    rib_ipv6_static_route2['NumberNexthops'] = '1'
+    rib_ipv6_static_route2['1.1'] = dict()
+    rib_ipv6_static_route2['1.1']['Distance'] = '1'
+    rib_ipv6_static_route2['1.1']['Metric'] = '0'
+    rib_ipv6_static_route2['1.1']['RouteType'] = 'static'
+
+    # Populate the expected FIB ("show ipv6 route") route dictionary for the
+    # route 5234:5234::1/128 and its next-hops.
+    route_ipv6_static_route2 = rib_ipv6_static_route2
+
+    sleep(15)
+
+    step("Verifying the IPv4 static routes via loopbacks on switch 1")
+
+    # Verify route 173.0.0.1/32 and next-hops in RIB, FIB and
+    # running-config
+    aux_route = route_ipv4_static_route1["Route"]
+    verify_show_ip_route(sw1, aux_route, 'static', route_ipv4_static_route1)
+    aux_route = rib_ipv4_static_route1["Route"]
+    verify_show_rib(sw1, aux_route, 'static', rib_ipv4_static_route1)
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=True,
+                                                    route='173.0.0.1/32',
+                                                    nexthop='1.1')
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=True,
+                                                    route='173.0.0.1/32',
+                                                    nexthop='11.11.11.13')
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=True,
+                                                    route='173.0.0.1/32',
+                                                    nexthop='41.41.41.43')
+
+    # Verify route 183.0.0.1/32 and next-hops in RIB, FIB and
+    # running-config
+    aux_route = route_ipv4_static_route2["Route"]
+    verify_show_ip_route(sw1, aux_route, 'static', route_ipv4_static_route2)
+    aux_route = rib_ipv4_static_route2["Route"]
+    verify_show_rib(sw1, aux_route, 'static', rib_ipv4_static_route2)
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=True,
+                                                    route='183.0.0.1/32',
+                                                    nexthop='1.1')
+
+    # Verify route 4234:4234::1/128 and next-hops in RIB, FIB and verify the
+    # presence of all next-hops in running-config
+    aux_route = route_ipv6_static_route1["Route"]
+    verify_show_ipv6_route(sw1, aux_route, 'static', route_ipv6_static_route1)
+    aux_route = rib_ipv6_static_route1["Route"]
+    verify_show_rib(sw1, aux_route, 'static', rib_ipv6_static_route1)
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=False,
+                                                    route='4234:4234::1/128',
+                                                    nexthop='1.1')
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=False,
+                                                    route='4234:4234::1/128',
+                                                    nexthop='100:100::6')
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=False,
+                                                    route='4234:4234::1/128',
+                                                    nexthop='4.1')
+
+    # Verify route 5234:5234::1/128 and next-hops in RIB, FIB and verify the
+    # presence of all next-hops in running-config
+    aux_route = route_ipv6_static_route2["Route"]
+    verify_show_ipv6_route(sw1, aux_route, 'static', route_ipv6_static_route2)
+    aux_route = rib_ipv6_static_route2["Route"]
+    verify_show_rib(sw1, aux_route, 'static', rib_ipv6_static_route2)
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=False,
+                                                    route='5234:5234::1/128',
+                                                    nexthop='1.1')
+
+
+# This test case does "no routing" in the parent L3 interface. This parent
+# interface contains the L3 sub-interface. "no routing" trigger in the
+# parent L3 interface will cause the L3 sub-interface to be deleted. The
+# IPv4 and IPv6 static routes and the next-hops routing via those L3
+# sub-interfaces should be deleted from DB.
+def no_routing_trigger_l3_parent_interfaces(sw1, sw2, step):
+
+    step("Entering interface for link 1 SW1, to execute 'no routing'")
+
+    # Make the L3 interface 1 as L2 by executing "no routing"
+    sw1("interface {}".format(sw1.ports["if01"]))
+    sw1("no routing")
+    sw1("exit")
+
+    # Populate the expected RIB ("show rib") route dictionary for the
+    # route 173.0.0.1/32 and its next-hops.
+    rib_ipv4_static_route1 = dict()
+    rib_ipv4_static_route1['Route'] = '173.0.0.1/32'
+    rib_ipv4_static_route1['NumberNexthops'] = '1'
+    rib_ipv4_static_route1['41.41.41.43'] = dict()
+    rib_ipv4_static_route1['41.41.41.43']['Distance'] = '1'
+    rib_ipv4_static_route1['41.41.41.43']['Metric'] = '0'
+    rib_ipv4_static_route1['41.41.41.43']['RouteType'] = 'static'
+
+    # Populate the expected FIB ("show ip route") route dictionary for the
+    # route 173.0.0.1/32 and its next-hops.
+    route_ipv4_static_route1 = rib_ipv4_static_route1
+
+    # Populate the expected RIB ("show rib") route dictionary for the
+    # route 183.0.0.1/32 and its next-hops.
+    rib_ipv4_static_route2 = dict()
+    rib_ipv4_static_route2['Route'] = '183.0.0.1/32'
+
+    # Populate the expected FIB ("show ip route") route dictionary for the
+    # route 183.0.0.1/32 and its next-hops.
+    route_ipv4_static_route2= rib_ipv4_static_route2
+
+    # Populate the expected RIB ("show rib") route dictionary for the
+    # route 4234:4234::1/128 and its next-hops.
+    rib_ipv6_static_route1 = dict()
+    rib_ipv6_static_route1['Route'] = '4234:4234::1/128'
+    rib_ipv6_static_route1['NumberNexthops'] = '1'
+    rib_ipv6_static_route1['4.1'] = dict()
+    rib_ipv6_static_route1['4.1']['Distance'] = '1'
+    rib_ipv6_static_route1['4.1']['Metric'] = '0'
+    rib_ipv6_static_route1['4.1']['RouteType'] = 'static'
+
+    # Populate the expected FIB ("show ipv6 route") route dictionary for the
+    # route 4234:4234::1/128 and its next-hops.
+    route_ipv6_static_route1 = rib_ipv6_static_route1
+
+    # Populate the expected RIB ("show rib") route dictionary for the
+    # route 5234:5234::1/128 and its next-hops.
+    rib_ipv6_static_route2 = dict()
+    rib_ipv6_static_route2['Route'] = '5234:5234::1/128'
+
+    # Populate the expected FIB ("show ipv6 route") route dictionary for the
+    # route 5234:5234::1/128 and its next-hops.
+    route_ipv6_static_route2 = rib_ipv6_static_route2
+
+    sleep(15)
+
+    step("Verifying the IPv4 static routes via loopbacks on switch 1")
+
+    # Verify route 173.0.0.1/32 and next-hops in RIB, FIB and
+    # running-config
+    aux_route = route_ipv4_static_route1["Route"]
+    verify_show_ip_route(sw1, aux_route, 'static', route_ipv4_static_route1)
+    aux_route = rib_ipv4_static_route1["Route"]
+    verify_show_rib(sw1, aux_route, 'static', rib_ipv4_static_route1)
+    assert not route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=True,
+                                                    route='173.0.0.1/32',
+                                                    nexthop='1.1')
+    assert not route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=True,
+                                                    route='173.0.0.1/32',
+                                                    nexthop='11.11.11.13')
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=True,
+                                                    route='173.0.0.1/32',
+                                                    nexthop='41.41.41.43')
+
+    # Verify route 183.0.0.1/32 and next-hops in RIB, FIB and
+    # running-config
+    aux_route = route_ipv4_static_route2["Route"]
+    verify_show_ip_route(sw1, aux_route, 'static', route_ipv4_static_route2)
+    aux_route = rib_ipv4_static_route2["Route"]
+    verify_show_rib(sw1, aux_route, 'static', rib_ipv4_static_route2)
+    assert not route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=True,
+                                                    route='183.0.0.1/32',
+                                                    nexthop='1.1')
+
+    # Verify route 4234:4234::1/128 and next-hops in RIB, FIB and verify the
+    # presence of all next-hops in running-config
+    aux_route = route_ipv6_static_route1["Route"]
+    verify_show_ipv6_route(sw1, aux_route, 'static', route_ipv6_static_route1)
+    aux_route = rib_ipv6_static_route1["Route"]
+    verify_show_rib(sw1, aux_route, 'static', rib_ipv6_static_route1)
+    assert not route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=False,
+                                                    route='4234:4234::1/128',
+                                                    nexthop='1.1')
+    assert not route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=False,
+                                                    route='4234:4234::1/128',
+                                                    nexthop='100:100::6')
+    assert route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=False,
+                                                    route='4234:4234::1/128',
+                                                    nexthop='4.1')
+
+    # Verify route 5234:5234::1/128 and next-hops in RIB, FIB and verify the
+    # presence of all next-hops in running-config
+    aux_route = route_ipv6_static_route2["Route"]
+    verify_show_ipv6_route(sw1, aux_route, 'static', route_ipv6_static_route2)
+    aux_route = rib_ipv6_static_route2["Route"]
+    verify_show_rib(sw1, aux_route, 'static', rib_ipv6_static_route2)
+    assert not route_and_nexthop_in_show_running_config(sw1=sw1,
+                                                    if_ipv4=False,
+                                                    route='5234:5234::1/128',
+                                                    nexthop='1.1')
+
+
 def test_zebra_ct_no_routing_trigger(topology, step):
     sw1 = topology.get("sw1")
     assert sw1 is not None
@@ -793,3 +1106,5 @@ def test_zebra_ct_no_routing_trigger(topology, step):
     add_static_routes(sw1, sw2, step)
     no_routing_trigger_static_routes(sw1, sw2, step)
     routing_trigger_static_routes(sw1, sw2, step)
+    add_static_routes_via_l3_sub_interfaces(sw1, sw2, step)
+    no_routing_trigger_l3_parent_interfaces(sw1, sw2, step)

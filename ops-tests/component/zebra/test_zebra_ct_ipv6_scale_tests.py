98f509781854554becfce04e9898a4fbd81b71af
Gaurav Gupta <gaurav.gupta4@hpe.com>
2016-07-11 16:32:11 -0700

    Static routes not removed from OVSDB when related interface is set to "no routing" in a scaled setup
    This fix addresses the following issues:-
    - In a scaled setup, ops-zebra won't remove all static routes if the resolving
      L3 port is made L2 by "no routing". This is because ops-zebra doesn't create
      a new transaction once it has published updates for 100 routes to OVSDB.
    - ops-zebra will crash in case a kernel route gets deleted due to NULL pointer
      de-refrence.
    - Enabling ops-zebra scaled IPV4 and IPV6 route test cases.
    
    Change-Id: I07ae43e6bcb80f3eff26cc30eacf1bc48b2fda10
    Signed-off-by: Gaurav Gupta <gaurav.gupta4@hpe.com>

--- ops-tests/component/zebra/test_zebra_ct_ipv6_scale_tests.py
+++ ops-tests/component/zebra/test_zebra_ct_ipv6_scale_tests.py
@@ -23,7 +23,6 @@ from route_generator_and_stats_reporter import (
     capture_output_samples_and_generate_perf_stats
 )
 from time import sleep
-from pytest import mark
 
 TOPOLOGY = """
 # +-------+    +-------+
@@ -319,7 +318,6 @@ def InterfaceNoRouting(sw1, sw2, ipv6_route_list, step):
            actual: " + str(perf_stats_dict['TotalShowRunning'])
 
 
-@mark.skipif(True, reason="Skipping scaled zebra IPv6 route tests")
 def test_zebra_ct_ipv6_scale_tests(topology, step):
     sw1 = topology.get("sw1")
     sw2 = topology.get("sw2")

a02de54116a484316e97195d85ea4ff826243e45
iyergan <ganesh.m.iyer@hpe.com>
2016-08-11 11:31:30 -0700

    chg:dev: BGP crashing when establishing adjency and the switch doesn't recover properly
    TG - 1739
    
    Change-Id: I2c505c93244c5ebaa6348754e881e90dfbeb7ff4
    Signed-off-by: iyergan <ganesh.m.iyer@hpe.com>

--- bgpd/bgp_ovsdb_route.c
+++ bgpd/bgp_ovsdb_route.c
@@ -611,11 +611,13 @@ bgp_ovsdb_set_local_rib_nexthop(struct ovsdb_idl_txn *txn,
     nexthop_list[0] = (struct ovsrec_bgp_nexthop *) pnexthop;
     nexthop_list[0]->ip_address = xstrdup(nexthop_buf);
     int ii = 1;
-    /* Set multipath nexthops */
-    for(mpinfo = bgp_info_mpath_first (info); mpinfo;
-        mpinfo = bgp_info_mpath_next (mpinfo))
+
+    if (nexthop_num > 1) {
+      /* Set multipath nexthops */
+        for(mpinfo = bgp_info_mpath_first (info); mpinfo;
+              mpinfo = bgp_info_mpath_next (mpinfo))
         {
-            /* Update the nexthop table. */
+                /* Update the nexthop table. */
             if (p->family == AF_INET) {
                 nexthop = &mpinfo->attr->nexthop;
                 if (nexthop->s_addr == 0) {
@@ -645,6 +647,7 @@ bgp_ovsdb_set_local_rib_nexthop(struct ovsdb_idl_txn *txn,
             nexthop_list[ii]->ip_address = xstrdup(nexthop_buf);
             ii++;
         }
+    }
     ovsrec_bgp_route_set_bgp_nexthops(rib, nexthop_list, nexthop_num);
     for (ii = 0; ii < nexthop_num; ii++)
         free(nexthop_list[ii]->ip_address);
@@ -1031,9 +1034,6 @@ bgp_lookup_global_hmap_entry(struct prefix *p,
         if(!strcmp(hmap_entry->prefix, pr) &&
                   (hmap_entry->table_type == BGP_ROUTE) &&
                         !strcmp(hmap_entry->next_hop, info->peer->host)){
-                if (hmap_entry->state != DB_SYNC) {
-                    return false;
-                }
                 return true;
         }
     }
@@ -1131,6 +1131,11 @@ bgp_ovsdb_add_local_rib_entry(struct prefix *p,
 
     /* Nexthops */
     nexthop_num = 1 + bgp_info_mpath_count (info);
+    /*BGP ROUTE table row should not have multiple nexthops */
+    if (nexthop_num > 1) {
+        nexthop_num = 1;
+    }
+
     VLOG_DBG("Setting nexthop num %d, metric %d, bgp_info_flags 0x%x\n",
              nexthop_num, info->attr->med, info->flags);
     /* Nexthop list */

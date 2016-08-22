8d3fa6a84b0250dde9800d8a35b5a8da9a25da7f
Nimisha Sara Mathews <nimisha.sara.mathews@hpe.com>
2016-08-22 12:16:06 -0700

    5712 BGP connection establishment delay on reboot TG-1818
    Tags: fix, usr
    
    Change-Id: Ic3fbf2fb5680bd33aab933c950bf5592fcda9607
    Signed-off-by: Nimisha Sara Mathews <nimisha.sara.mathews@hpe.com>

--- bgpd/bgpd.h
+++ bgpd/bgpd.h
@@ -745,7 +745,7 @@ struct bgp_nlri
 #define BGP_EVENTS_MAX                          15
 
 /* BGP timers default value.  */
-#define BGP_INIT_START_TIMER                     5
+#define BGP_INIT_START_TIMER                    10
 #define BGP_ERROR_START_TIMER                   30
 #define BGP_DEFAULT_HOLDTIME                   180
 #define BGP_DEFAULT_KEEPALIVE                   60 
